bbc5999485a80110ec41836728dfb731b352420a
Bakamashine <surajaaminu09@gmail.com>
2026-02-04 09:52:57 +0300

    fix bug

--- main.py
+++ main.py
@@ -78,6 +78,7 @@ if __name__ == "__main__":
     for index, item in themes.items():
         if currentDate == index:
             message = f'Здравствуйте. К практике приступил с 9:00 по 14:00, работаю над темой "{item}"'
+            break
         else:
             message = "Здравствуйте. Сегодня продолжаю делать прошлую тему."
     print(f"Message result: {message}")
